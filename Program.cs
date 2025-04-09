8d3954c7012f4403230fe7919865770d0e73a52f
kartikey-udainiya <kartikeyudainiya@gmail.com>
2025-04-09 13:57:42 +0530

    Fresh commit without heavy files

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            driver.Navigate().GoToUrl("https://app.cloudqa.io/home/AutomationPracticeForm");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+
+            var firstName = driver.FindElement(By.Name("First Name"));
+            firstName.SendKeys("John");
+
+            var lastName = driver.FindElement(By.Name("Last Name"));
+            lastName.SendKeys("Doe");
+
+            var email = driver.FindElement(By.Name("Email"));
+            email.SendKeys("johndoe@gmail.com");
+        
+            Console.WriteLine("Fields filled successfully!");
+            Thread.Sleep(3000);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+}
